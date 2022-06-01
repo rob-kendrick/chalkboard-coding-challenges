fcac530f13c72bcfb22c92cee9ce35f0ffbd854a
rob-kendrick <kendrickrobert99@gmail.com>
2022-06-01 15:08:48 +0200

    UserList : added basic code, moving on to userListItem

--- birthdays/src/Components/UserList/UserList.js
+++ birthdays/src/Components/UserList/UserList.js
@@ -1,7 +1,16 @@
 import React from "react";
+import UserListItem from "../UserListItem/UserListItem";
+import "./UserList.css";
 
-function UserList() {
-  return <div>UserList</div>;
+function UserList({ users }) {
+  return (
+    <div className="UserList__container">
+      <h1 className="UserList__Header">Birthdays</h1>
+      {users.map((item) => {
+        //return userListItem
+      })}
+    </div>
+  );
 }
 
 export default UserList;
