b92e77a0c5416bbea6e7ddc5bffbe1baf6f4366a
rob-kendrick <kendrickrobert99@gmail.com>
2022-06-03 13:26:21 +0200

    Minor UI Changes for fun. Got rid of some redundant CSS

--- birthdays/src/services/apiService.test.js
+++ birthdays/src/services/apiService.test.js
@@ -2,6 +2,8 @@ import { getUsers } from "./apiService";
 
 // GetUsers takes the original object returned from API, adds 'id' properties to each item, and returns the final modified array.
 
+// We could test this with mock data, but the random API data may change, so it is better to test for the kind of data we get back.
+
 test("API Service: Returns Array", async () => {
   const response = await getUsers();
   expect(response).toBeInstanceOf(Array);
